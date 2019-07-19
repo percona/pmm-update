e24fddbc610d4023b8bbc7b6d051a565826a91b7
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-07-19 18:48:53 +0300

    PMM-4391 WIP

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -3,9 +3,10 @@
 set -o errexit
 set -o xtrace
 
-yum install -y golang mc tmux
+yum install -y golang mc tmux which
 go env
 
-go get -u -v golang.org/x/tools/cmd/gopls
+go get -u -v golang.org/x/tools/cmd/gopls \
+    github.com/acroca/go-symbols
 
 #curl https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $GOPATH/bin
