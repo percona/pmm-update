df3fb9c7c27458906729789d79c53453861a7d2a
Nikita Beletskii <nikita.beletskii@percona.com>
2021-12-24 16:47:00 +0300

    PMM-9374 use external victoria metrics

---
+++ ansible/playbook/tasks/roles/nginx/files/nginx.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+export VICTORIAMETRICS_HOST=${VICTORIAMETRICS_HOST:-127.0.0.1:9091}
+envsubst < /etc/nginx/conf.d/pmm.conf.template > /etc/nginx/conf.d/pmm.conf
+
+exec nginx
